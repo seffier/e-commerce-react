804df651464e3f9cc79f5f755d90efe94df9bcf9
Sefer <sefer865@hotmail.com>
2024-01-18 13:57:36 +0300

    login formik validation added

--- src/components/Custom/Layouts/Authentication/Layout/leftContainer.styled.ts
+++ src/components/Custom/Layouts/Authentication/Layout/leftContainer.styled.ts
@@ -8,7 +8,7 @@ export const LeftContainer = styled(Grid)(({ theme }) => ({
   backgroundPosition: 'center',
   backgroundRepeat: 'no-repeat',
   backgroundSize: 'cover',
-  backgroundImage: 'url(\'https://img.freepik.com/premium-vector/friendly-volunteer-feeding-dog-street-animal-shelter-pound-young-man-giving-bowl-homeless-puppy_1016-13733.jpg?w=740\')',
+  backgroundImage: 'url(\'https://img.freepik.com/free-photo/laptop-blank-screen-hopping-cart-full-gifts-with-copyspace-online-shopping-concept_1423-89.jpg?w=1380&t=st=1705495655~exp=1705496255~hmac=36249bd06af1905047f903bbb6112005316ed6680d5b5e433822d3b8f70d22f8\')',
   position: 'sticky',
   top: 0,
   [theme.breakpoints.down('sm')]: {
